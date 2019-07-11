d4d8f023508bca98819dcaf88bb02a3da57a526e
18631323023 <qq492690314>
2019-07-11 10:30:13 +0800

    房间操作记录

---
+++ src/main/java/com/szq/hotel/service/RoomRecordService.java
@@ -0,0 +1,30 @@
+package com.szq.hotel.service;
+
+import com.szq.hotel.dao.RoomRecordDAO;
+import com.szq.hotel.dao.TestDAO;
+import com.szq.hotel.entity.bo.RoomRecordBO;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2018/2/28.
+ */
+@Service("roomRecordService")
+@Transactional
+public class RoomRecordService {
+    @Resource
+    private RoomRecordDAO roomRecordDAO;
+
+    /*
+        添加房间操作记录
+    */
+    public int insert(RoomRecordBO record, Integer userId,Date now){
+        now = new Date();
+        record.setCreateUserId(userId);
+        record.setCreateTime(now);
+        return roomRecordDAO.insert(record);
+    }
+}
