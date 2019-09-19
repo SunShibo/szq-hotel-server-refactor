90c87162ee9cae3d40b3b253b21c00c134e12ca0
wy <18731612878@163.com>
2019-09-19 09:57:04 +0800

    补充日志

--- src/main/java/com/szq/hotel/util/redisUtils/RedissonHandler.java
+++ src/main/java/com/szq/hotel/util/redisUtils/RedissonHandler.java
@@ -22,7 +22,7 @@ public class RedissonHandler {
 
     private final static String ip = "60.205.226.180";//"60.205.226.180";//"wisewin-tech.com";
 
-    private final static String password = "denglu328325";//"denglu328325";//"sunshibo1!";
+    private final static String password = "Sunshibo1!";//"denglu328325";//"sunshibo1!";
 
 
 
