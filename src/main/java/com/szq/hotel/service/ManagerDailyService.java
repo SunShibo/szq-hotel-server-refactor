0a1d7e16d5def1bd82054e79650f172ba890537f
wangyang <18731612878@163.com>
2019-12-18 14:07:42 +0800

    修改需求、redis\mysql连接

--- src/main/java/com/szq/hotel/service/ManagerDailyService.java
+++ src/main/java/com/szq/hotel/service/ManagerDailyService.java
@@ -6,7 +6,6 @@ import com.szq.hotel.dao.ManagerdailyBoDAO;
 import com.szq.hotel.entity.bo.*;
 import com.szq.hotel.util.DateUtils;
 import org.apache.commons.collections4.CollectionUtils;
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
